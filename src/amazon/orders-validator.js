1e8305f2596c9f9f313fee6f5dbe5decc5ca59cb
ansarianas <ansarianas1897@gmail.com>
2024-01-09 21:26:39 +0530

    Count total parsed orders

---
+++ src/amazon/orders-validator.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+
+const fileName = '';
+const data = fs.readFileSync(fileName, 'utf8');
+const parsed = JSON.parse(data);
+console.log(`Total processed orders - ${parsed.length}`);
