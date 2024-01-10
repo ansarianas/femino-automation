6e939dc1ebfe9bcf1fae1732630c3fc49c595499
ansarianas <ansarianas1897@gmail.com>
2024-01-10 12:44:52 +0530

    Added relative path to orders

--- src/utils/orders-validator.js
+++ src/utils/orders-validator.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
 const fileName = '';
-const data = fs.readFileSync(fileName, 'utf8');
+const data = fs.readFileSync(`../../data/amazon/orders/${fileName}`, 'utf8');
 const parsed = JSON.parse(data);
 console.log(`Total processed orders - ${parsed.length}`);
